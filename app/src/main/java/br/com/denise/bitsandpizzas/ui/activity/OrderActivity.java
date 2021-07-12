e68d7a43fc35147cd450b7a80b5bbb974effc42d
Denise Castro <dmaria.natal@gmail.com>
2021-07-12 18:16:21 -0300

    Criando a OrderActivity

--- app/src/main/java/br/com/denise/bitsandpizzas/ui/activity/OrderActivity.java
+++ app/src/main/java/br/com/denise/bitsandpizzas/ui/activity/OrderActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 
 import br.com.denise.bitsandpizzas.R;
 
-public class MainActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order);
 
         //Adicionando a toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
