010e8965cbba13145fde7f8987bd8659842c7cee
Denise Castro <dmaria.natal@gmail.com>
2021-07-12 11:06:44 -0300

    Iniciando o projeto para pedido de pizzas no android

---
+++ app/src/main/java/br/com/denise/bitsandpizzas/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.denise.bitsandpizzas.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.denise.bitsandpizzas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
