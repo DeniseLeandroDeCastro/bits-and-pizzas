50f5bbc30a367ec3f1589eb8bd33b3effbc1e887
Denise Castro <dmaria.natal@gmail.com>
2021-07-12 22:37:33 -0300

    Item compartilhar

--- app/src/main/java/br/com/denise/bitsandpizzas/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/denise/bitsandpizzas/ui/activity/MainActivity.java
@@ -8,11 +8,15 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import br.com.denise.bitsandpizzas.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Quer se juntar a mim para uma pizza?");
         return super.onCreateOptionsMenu(menu);
+    }
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
     //Ação da barra do app quando recebe um clique
     @Override
